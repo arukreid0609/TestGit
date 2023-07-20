b9d566ed52669852db9bb751f3bd9c03c47bbec1
sasakiy916 <sasaki3y98521j@gmail.com>
2023-07-21 01:05:46 +0900

    head

--- Main5.java
+++ Main5.java
@@ -1,9 +1,9 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("笹木さｋさき");
+
 		System.out.println("笹木さｋさき");
-		System.out.println("笹木さｋさき");
-		System.out.println("笹木さｋさき");
+
 		System.out.println("笹木さｋさき");
 		System.out.println("");
 	}
