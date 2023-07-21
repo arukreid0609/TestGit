353cf11a1ca2c2e6706f9d03aa8e48b4bd011d9e
sasakiy916 <sasaki3y98521j@gmail.com>
2023-07-21 11:23:17 +0900

    佐々木が頑張って練習

--- Main5.java
+++ Main5.java
@@ -1,7 +1,9 @@
 public class Main{
 	public static void main(String[] args){
-		System.out.println("笹木さｋさき");
+		System.out.println("笹木さｋさきaaaaa");
 
+		if(5<6){
+	  }
 		System.out.println("笹木さｋさき");
 
 		System.out.println("笹木さｋさき");
